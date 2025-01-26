2b552c73f8a5077cb5300b5fcbb4fa0c7f59c2f6
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-01-26 20:26:39 +0000

    Fixed title

--- report-website/app/ui-design/page.tsx
+++ report-website/app/ui-design/page.tsx
@@ -4,7 +4,7 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbP
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Research',
+    title: 'UI Design',
   };
 
 export default function UIDesign() {
@@ -48,7 +48,7 @@ export default function UIDesign() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div id="final-design">
+          <div id="production-design">
             <h1 className="text-4xl font-bold my-6">Production Design</h1>
             <p className="text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
