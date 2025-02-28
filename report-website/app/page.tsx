537b5293a359c1ecbacfb4647539172938830733
248abdul <abdulhamidabayomi123@gmail.com>
2025-02-28 02:56:52 +0000

    added comment about Gantt-Chart

--- report-website/app/page.tsx
+++ report-website/app/page.tsx
@@ -85,8 +85,7 @@ export default function Home() {
           <div id="project-management">
             <h1 className="text-4xl font-bold my-6">Project Management</h1>
             <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            (Will create Gantt-Chart)            </p>
           </div>
         </div>
       </div>
