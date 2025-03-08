49f27825d705da0c410895ad9f58315f1a8272ae
248abdul <abdulhamidabayomi123@gmail.com>
2025-03-08 20:00:45 +0000

    update requirements

--- report-website/app/requirements/page.tsx
+++ report-website/app/requirements/page.tsx
@@ -96,7 +96,7 @@ export default function Requirements() {
               <li>
                 <strong>User Surveys and Feedback</strong>
                 <ul className="list-disc my-3 pl-6">
-                  <li>We conducted structured surveys to assess the challenges with traditional authentication.</li>
+                  <li>We conducted structured surveys via questionnaires to assess the challenges with traditional authentication.</li>
                   <li>All participants were asked to sign a research participation consent form, which outlined the details of the survey to participants and allowed them to make an informed decision on whether to participate.</li>
                   <li>Participants were asked about hot-desking difficulties, privacy concerns, and preferred authentication methods.</li>
                   <li>We used a combination of open and closed questions, allowing us to gather both detailed, qualitative insights from open-ended questions while also getting quantifiable data and clear structure from closed-ended questions.</li>
@@ -121,6 +121,29 @@ export default function Requirements() {
             <p className="text-lg">
               Our user survey produced valuable insights:
             </p>
+              <ul className="list-disc pl-6 my-6">
+              <li>
+                <strong>67% of respondents stated that they had trouble with hot-desking, and found manual logins inconvenient and time-consuming.</strong>
+              </li>
+              <ul className="list-disc my-6">
+              <li>
+                <strong>100% of respondents suggested that having a mechanism to automatically load up your profile and settings would be helpful, with many commenting that "it would save time by eliminating the need to log in and configure settings manually, making the process smoother and faster, especially when switching between devices".</strong>
+                </li>
+              </ul>
+                <ul className="list-disc my-6">
+              <li>
+                <strong>Users with accessibility needs strongly supported AI-driven chatbot assistance to modify settings, noting that this would be "a great feature".</strong>
+              </li>
+              </ul>
+              <ul className="list-disc my-6">
+              <li>
+                When asked specifically whether a proximity-based mechanism for authentication and access to profiles would be useful, <strong>67% of respondents agreed, with one stating that "[the mechanism] sounds very futuristic; it would help people with accessibility challenges all around the world access essential services on their devices”.</strong> We believe that this figure will be significantly higher once more users are educated on how the mechanism works and how it can be used, as one respondent selected the "I'm not sure" option, suggesting they may not have known what the mechanism entails.
+              </li>
+              </ul>
+            </ul>
+            <h1 className="text-2xl font-bold my-6">Evaluation of Survey Methods</h1>
+            <p className="text-lg">
+              The survey conducted to gather user requirements for the Quantum Proximity Gateway (QPG) was structured to identify key pain points, user expectations, and feature priorities. It included a mix of quantitative (multiple-choice) and qualitative (open-ended) questions, which allowed us to collect both statistical insights and deeper user opinions.            </p>
               <ul className="list-disc pl-6 my-6">
               <li>
                 <strong>67% of respondents stated that they had trouble with hot-desking, and found manual logins inconvenient and time-consuming.</strong>
