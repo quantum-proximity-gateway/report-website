f95c02c6b6f63ee6e20384066c67c2f287f9db32
Marwan <yassinimarwan6@gmail.com>
2025-03-23 17:09:25 +0000

    added ibm cloud architecture

--- report-website/app/system-design/page.tsx
+++ report-website/app/system-design/page.tsx
@@ -59,6 +59,25 @@ export default function SystemDesign() {
             <p className="text-lg mb-4">
               All these components come together to create a complex, scalable and secure architecture, allowing future upgrades and the integration of new proximity agent iterations. We reduce our reliance on the device being authenticated into by utilising a Raspberry Pi which allows developers to easily adapt this system for other usecases (i.e printers, whiteboard profiles).
             </p>
+            <h1 className="text-2xl font-medium  my-6">IBM Cloud System Architecture</h1>
+            <p className="text-lg mb-4">
+              Both our registration website and API are hosted utilising cloud services, this is the first step to getting a QPG system setup. For our server, we are making use of serverless computing utilising IBM's Cloud Code Engine, which allows us to deploy a flexible and scalable server. This helps us ensure the costs of such a system are reasonable and that the services are accessible at all times. IBM Cloud Code Engine automatically scales the number of instances of the server that we have running just by the number of requests that we are getting, so if there are no requests incoming no instances of the server will be initiated and vice-versa.
+            </p>
+            <div className="flex flex-col items-center my-6">
+              <Image 
+                src="/system-design/codeengine-architecture.svg" 
+                alt="IBM Cloud Engine Architecture" 
+                width={800} 
+                height={800}
+                className="max-w-full object-contain bg-white"
+                style={{borderRadius:'10px'}}
+              />
+              <p className="text-center mt-3 text-sm text-gray-500">Figure 2: IBM Code Engine Architecture Diagram</p>
+              <p className="text-center text-sm text-gray-500">https://cloud.ibm.com/docs/codeengine?topic=codeengine-architecture</p>
+            </div>
+            <p className="text-lg mb-4">
+              By containerizing our Litestar server onto a Docker container, we ensured that our deployment options were flexible too, allowing us to easily update the server or switch to other cloud providers in the future if needed.
+            </p>
           </div>
           <div id="site-map">
             <h1 className="text-4xl font-bold my-6">Site Map</h1>
