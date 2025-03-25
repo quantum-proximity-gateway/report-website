2528654284466e1523c893902f20f88e9c9000e4
Marwan <yassinimarwan6@gmail.com>
2025-03-25 02:06:49 +0000

    added code snippet

--- report-website/app/testing/page.tsx
+++ report-website/app/testing/page.tsx
@@ -1,13 +1,21 @@
+'use client'
+
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { Separator } from "@/components/ui/separator"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    title: 'Testing',
-  };
+import Prism from 'prismjs';
+import 'prismjs/components/prism-python';
+import 'prismjs/themes/prism-okaidia.css';
+import { useEffect } from 'react';
 
 export default function Testing() {
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      Prism.highlightAll();
+    }
+  }, []);
+
   return (
     <main className="w-full">
       <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -47,19 +55,20 @@ export default function Testing() {
             <p className="text-lg my-6">
               The test suite uses pytest's fixture mechanism for dependency injection, this helps us easily create test clients for our server before every test, inject them into the test function as a dependency and automatically handle the test cleanup.
             </p>
-            <pre>
+            <pre className="bg-gray-900 p-4 rounded-md overflow-x-auto">
               <code className="language-python">
-                {`
-                @pytest_asyncio.fixture(scope='function')
-                async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
-                    async with AsyncTestClient(app=app) as client:
-                        yield client
+                {`@pytest_asyncio.fixture(scope='function')
+async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
+    async with AsyncTestClient(app=app) as client:
+        yield client
 
-                    # Delete test database after each test
-                    os.remove(TEST_DB_FILENAME)
-                `}
+    # Delete test database after each test
+    os.remove(TEST_DB_FILENAME)`}
               </code>
             </pre>
+            <p className="text-lg my-6">
+              Having a large and critical part of our project like the core server being unit tested allows us to easily improve functionality in future iteration without the worries of additional bugs and vulnerabilities being introduced.
+            </p>
           </div>
           <div id="integration-testing">
             <h1 className="text-4xl font-bold my-6">Integration Testing</h1>
@@ -88,7 +97,7 @@ export default function Testing() {
           <div id="user-acceptance-testing">
             <h1 className="text-4xl font-bold my-6">User Acceptance Testing</h1>
             <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              
             </p>
           </div>
         </div>
