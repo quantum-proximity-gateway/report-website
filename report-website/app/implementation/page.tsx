5cdc626e38021f2b3f25df1fd6850156e73e3ea4
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-25 15:04:58 +0000

    Updated implementation

--- report-website/app/implementation/page.tsx
+++ report-website/app/implementation/page.tsx
@@ -33,7 +33,10 @@ export default function Implementation() {
           <div id="overview">
             <h1 className="text-4xl font-bold my-6">Overview</h1>
             <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              We used GitHub becauase... created branches, issues, and pull requests...
+            </p>
+            <p className="text-lg my-6">
+              There are 5 main repositories...
             </p>
           </div>
           <div id="server">
