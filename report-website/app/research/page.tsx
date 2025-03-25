e4bbf8f6575e7501a468271a7566de999ab8be55
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-25 15:14:25 +0000

    Removed time-based one-time password from research page

--- report-website/app/research/page.tsx
+++ report-website/app/research/page.tsx
@@ -286,10 +286,6 @@ export default function Research() {
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
-            <h1 className="text-2xl font-bold my-6">Time-Based One-Time Password</h1>
-            <p className="text-lg my-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>  
           </div>
           <div id="technical-decisions">
             <h1 className="text-4xl font-bold my-6">Technical Decisions</h1>
