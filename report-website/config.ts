9f192c9550fa5e9a767c26be9aa983c1505d4590
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-03-29 11:04:42 +0000

    Edited config comment

--- report-website/config.ts
+++ report-website/config.ts
@@ -3,5 +3,5 @@ export const BASE_PATH = "/";
 // export const BASE_PATH = "/2024/group10/";
 
 // !!! VERY IMPORTANT !!!
-// In the static out/ folder you MUST add the BASE_PATH value before /_next/ (i.e. /2024/group10/_next/path/to/file) in all src links
-// In VS Code, open the out/ folder, do Ctrl-Shift-F, and replace all instances of /_next/ with /2024/group10/_next/
+// In the static out/ folder you MUST CORRECT the BASE_PATH value before _next/ in all src links. By default the base path is / (/_next/).
+// In VS Code, open the out/ folder, do Ctrl-Shift-F, and replace all instances of /_next/ with your adjusted path, i.e. /2024/group10/_next/
